ae53cd69b50b6d6c25a3f8a4fd83cd58e712ca47
Daniel Tealdi <daniel@danieltealdi.com>
2024-03-28 20:54:53 +0100

    Hasta 712

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -10,31 +10,34 @@ class PonentesController
 {
     public static function index(Router $router)
     {
-        //debuguear('aqui');
+        $ponentes = Ponente::all();
+        //$ponentes = [];
+        //debuguear($ponentes);
         $router->render('admin/ponentes/index', [
             'titulo' => 'Ponentes / Conferencistas',
+            'ponentes' => $ponentes
         ]);
     }
     public static function crear(Router $router)
     {
         //leer imagen
         if(!empty($_FILES['imagen']['tmp_name'])) {
-            $carpetaImagenes='../public/img/ponentes';
+            $carpetaImagenes = '../public/img/ponentes';
             //debuguear ($carpetaImagenes);
             if(!is_dir($carpetaImagenes)) {
                 //debuguear('no existe');
                 //esto no funciona debe cambiarse el usuario de la carpeta
                 //chown -R www-data:www-data public/
-                $resultado=mkdir($carpetaImagenes, 0775, true);
+                $resultado = mkdir($carpetaImagenes, 0775, true);
                 //debuguear(__DIR__.'/'.$carpetaImagenes);
             }
             //debuguear('ya existe');
             $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
             $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
             //debuguear($imagen_png);
-            $nombre_imagen=md5(uniqid(rand(), true));
+            $nombre_imagen = md5(uniqid(rand(), true));
             //debuguear($nombre_imagen);
-            
+
             $_POST['imagen'] = $nombre_imagen;
         }
         $alertas = [];
@@ -49,7 +52,7 @@ class PonentesController
             $alertas = $ponente->validar();
             //debuguear($alertas);
             if(empty($alertas)) {
-               
+
                 $imagen_png->save($carpetaImagenes.'/'.$nombre_imagen.'.png');
                 $imagen_webp->save($carpetaImagenes.'/'.$nombre_imagen.'.webp');
                 $resultado = $ponente->guardar();
@@ -61,6 +64,24 @@ class PonentesController
         $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponente',
             'alertas' => $alertas,
+            'ponente' => $ponente ?? null
+        ]);
+    }
+    public static function editar(Router $router)
+    {
+        $alertas = [];
+        $ponente = '';
+        $id=$_GET['id'];
+        $id=filter_var($id, FILTER_VALIDATE_INT);
+        if(!$id){
+            header('Location: /admin/ponentes');
+        }
+        $ponente = Ponente::find($id);
+        if(!$ponente){
+            header('Location: /admin/ponentes');
+        }
+        $router->render('admin/ponentes/editar', [
+            'titulo' => 'Editar Ponente',
             'ponente' => $ponente
         ]);
     }
--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -5,3 +5,40 @@
     <i class="fa-solid fa-circle-plus"></i>    
     Registrar Ponente</a>    
 </div>
+<div class="dashboard__contenedor">
+    <?php if(count($ponentes) === 0) { ?>
+        <p class="text-center">No Hay Ponentes Aún</p>
+    <?php } else { ?>
+        <table class="table">
+            <thead class="table__thead">
+                <tr>
+                    <th scope="col" class="table__th">Nombre</th>
+                    <th scope="col" class="table__th">Ubicación</th>
+                    <th scope="col" class="table__th"></th>
+                </tr>
+            </thead>
+            <tbody class="table__tbody">
+                <?php foreach($ponentes as $ponente) { ?>
+                    <tr class="table__tr">
+                        <td class="table__td">
+                            <?php echo $ponente->nombre . ' ' . $ponente->apellido; ?>
+                        </td>
+                        <td class="table__td">
+                            <?php echo $ponente->ciudad . ', ' . $ponente->pais; ?>
+                        </td>
+                        <td class="table__td--acciones">
+                            <a class="table__acciones table__acciones--editar" href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>" class="table__boton">
+                                <i class="fa-solid fa-user-pen"></i>
+                                Editar</a>
+                            <form class="table__formulario">
+                                <button class="table__acciones table__acciones--eliminar" type="submit">
+                                    <i class="fa-solid fa-circle-xmark"></i>
+                                    Eliminar
+                                </button>
+                                <input type="hidden" name="id" value="<?php echo $ponente->id; ?>">
+                            </form>
+                        </td>
+                        
+            <?php } ?>
+            </table>
+    <?php } ?>
